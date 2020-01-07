62d0e6e87cccc8684e824ea224a3b0f2ee2c4531
Alejandra Roel <alejandraroel@Alejandras-MacBook-Air-2.local>
2020-01-06 21:31:52 -0500

    First Commit

---
+++ Ale2020.2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+
+namespace Ale2020
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string vacationType;
+
+            int groupSize;
+
+            string travelSuggestion = null;
+
+            string destination = null;
+
+            string result;
+
+
+            Console.WriteLine("What kind of trip would you like to go on musical,tropical, or adventurous? ");
+
+            vacationType = Console.ReadLine();
+
+
+            if (vacationType == "musical")
+            {
+                destination = "new orleans";
+
+            }
+            else if (vacationType == "tropical")
+            {
+                destination = "Beach Vacation in Mexico";
+            }
+
+            else if (vacationType == "adventurous")
+            {
+                destination = "Whitewater Rafting the Grand Canyon";
+            }
+            else
+                Console.WriteLine("not a valid vacation type");
+
+
+
+            Console.WriteLine("How Many are in your group?");
+
+
+
+            groupSize = Convert.ToInt32(Console.ReadLine());
+
+
+            if (groupSize > 0 && groupSize <= 2)
+            {
+                travelSuggestion = "first class";
+            }
+
+            else if (groupSize >= 3 && groupSize <= 5)
+            {
+                travelSuggestion = "Helicopter";
+            }
+            else
+                travelSuggestion = "Charter flight";
+
+
+
+
+
+
+            result = $" Since you are a group of {groupSize} going on a {vacationType} vacation, you should take a {travelSuggestion} to {destination}";
+
+            Console.WriteLine(result);
+
+
+
+
+        }
+    }
+}
